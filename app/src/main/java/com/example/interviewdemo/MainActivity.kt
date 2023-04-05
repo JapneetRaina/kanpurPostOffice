5b3b4d7bc1f22d521bc029e09c34eeacc57fb232
Japneet Raina <japneet.raina@globtier.co.in>
2023-04-05 19:50:48 +0530

    Done

--- app/src/main/java/com/example/interviewdemo/MainActivity.kt
+++ app/src/main/java/com/example/interviewdemo/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.example.interviewdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
 import retrofit2.Response
-import javax.security.auth.callback.Callback
 
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
+    private lateinit var progressBar: ProgressBar
     private var pincodeResponse  =  (mutableListOf <PostOfficeList>())
     private var adapter =  RecyclerViewAdapter(this@MainActivity, pincodeResponse)
     val TAG = "MainActivity"
@@ -21,6 +24,8 @@ class MainActivity : AppCompatActivity() {
         initViews()
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+        progressBar.visibility = View.VISIBLE
+
         getPinCode()
     }
 
@@ -31,15 +36,19 @@ class MainActivity : AppCompatActivity() {
                 call: Call<List<PincodeResponse>?>,
                 response: Response<List<PincodeResponse>?>
             ) {
+                progressBar.visibility = View.GONE
                 pincodeResponse.clear()
                 val pincode = response.body()
                 if (response.isSuccessful) {
+                    Toast.makeText(this@MainActivity, pincode!![0].Status, Toast.LENGTH_SHORT).show()
                     pincodeResponse.addAll(pincode!![0].PostOffice)
                     adapter.notifyDataSetChanged()
                 }
             }
 
             override fun onFailure(call: Call<List<PincodeResponse>?>, t: Throwable) {
+                progressBar.visibility = View.GONE
+                Toast.makeText(this@MainActivity, "Failed", Toast.LENGTH_SHORT).show()
                 Log.d(TAG, "onFailure: ${t.message}")
             }
         })
@@ -47,5 +56,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun initViews() {
         recyclerView = findViewById(R.id.recyclerView)
+        progressBar = findViewById(R.id.progressBar)
     }
 }
